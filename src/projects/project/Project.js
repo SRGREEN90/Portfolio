342e4a9865bcabbe42d3f9b8cfc51e0d13f99fc6
SRGREEN90 <you@example.com>
2022-02-20 20:51:03 +0300

    mixins

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,22 +1,21 @@
 import React from "react";
-import s from './Project.module.css';
+import s from './Project.module.scss';
 
 
 function Project(props) {
-    return <div className={s.work}>
+    return <div className={s.project}>
 
         <div className={s.imageBlock} style={props.style}>
-            <a className={s.icon}>
-                {props.title}
-            </a>
+            <a className={s.vewBtn}>{props.title}</a>
         </div>
-        <h3 className={s.projectName}>
-            My <span className={s.colorTheme}>{props.projectName}</span>
-        </h3>
 
-        <span className={s.description}>
-            {props.description}
-            </span>
+        <div className={s.projectInfo}>
+            <h3 className={s.projectName}>
+                My <span className={s.colorTheme}>{props.projectName}</span>
+            </h3>
+            <span className={s.description}>{props.description}</span>
+        </div>
+
 
     </div>;
 }
